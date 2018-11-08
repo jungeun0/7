6d3a8cdab09e4e9eb3a4a3bffe51b70abb38cb47
jungeun0 <sky_fall7@naver.com>
2018-11-09 01:20:37 +0900

    07-03

--- main.c
+++ main.c
@@ -6,17 +6,13 @@
 int main(int argc, char *argv[]) {
 	
 	int x = 10;
-	int y = 20;
 	int *ptr;
 	
 	ptr = &x;
-	
 	printf("ptr value : %d\n", *ptr);
 	
-	ptr = &y;
-	
-	printf("ptr value : %d\n", *ptr);
-	
+	*ptr = 20;
+	printf("x value : %d\n", x);
 	
 	return 0;
 }
