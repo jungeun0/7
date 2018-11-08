9d10c9b98a55ce9817713e160a3e4436ac997620
jungeun0 <sky_fall7@naver.com>
2018-11-09 01:58:20 +0900

    07-07(swap 변수)
    swap 함수에서 x와 y를 바꾸는 것 입니다!

--- main.c
+++ main.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void swap (int *x, int *y){
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;	
+}
+
+
 
 int main(int argc, char *argv[]) {
 	
-	 int i,j,k;
-	 int grade[5];
-	 int sum=0;
-	 int *ptr;
-	 ptr = grade;
-	  
+	 int a;
+	 int b;
 	 
-	 for(i=0;i<5;i++){
-	 	printf("input value (%d) = ", i);
-	 	scanf("%d", &grade[i]);
-	 }
-	 for(j=0;j<5;j++){
-	 	printf("grade[%d] : %d \n", j, grade[j]);
-	 }
-	 for(k=0;k<5;k++){
-	 	sum = *(ptr+k) + sum;	
-	 }
-	
-	
-	printf("average : %d\n",sum/i); 
+	 printf("x :  ");
+	 scanf("%d",&a);
+	 
+	 printf("y :  ");
+	 scanf("%d", &b);
+	 
+	 swap(&a,&b);
+	 
+	 printf("x : %d y : %d" ,a,b);
+	 
 	return 0;
 }
